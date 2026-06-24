2dbe7358f43f4897c9dba1e66652f764a13bb498
Johannes Roos <jhnnsrs@gmail.com>
2026-06-24 18:31:11 +0200

    fix: configuration update

--- kraph_server/settings_test.py
+++ kraph_server/settings_test.py
@@ -2,6 +2,6 @@ from .settings import *  # noqa
 from .settings import DATABASES, AUTHENTIKATE
 
 DATABASES["default"] = {**DATABASES["default"], "NAME": "testdb", "PORT": 5555, "HOST": "localhost", "USER": "test", "PASSWORD": "test"}
-AUTHENTIKATE = {**AUTHENTIKATE, "STATIC_TOKENS": {"test": {"sub": "1"}}}
+AUTHENTIKATE = {**AUTHENTIKATE, "static_tokens": {"test": {"sub": "1"}}}
 DATALAYER = {"media": {"path": "/tmp/datalayer_test", "jwt_key": "testkey"}}
 DATALAYER_URL = "http://testserver/datalayer"
